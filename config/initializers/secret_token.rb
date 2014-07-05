67a027bb31c537ccc7dd2d64d018472b09c36703
katorie <katoriexxxkatorie@gmail.com>
2014-07-05 20:06:16 +0900

    modify secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,27 @@
 # Be sure to restart your server when you modify this file.
+#
+# # Your secret key is used for verifying the integrity of signed cookies.
+# # If you change this key, all old signed cookies will become invalid!
+#
+# # Make sure the secret is at least 30 characters and all random,
+# # no regular words or you'll be exposed to dictionary attacks.
+# # You can use `rake secret` to generate a secure secret key.
+#
+# # Make sure your secret_key_base is kept private
+# # if you're sharing your code publicly.
+require 'securerandom'
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RssReader::Application.config.secret_token = 'c17bae6216acd4ee2e99fa079999771984f16607e3dcbfa6fd7a91259a7e5c6872b524dba87b5cfa044ee3322b62f9ba55890bf2035af85b8fb943ad9c76fb99'
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+RssReader::Application.config.secret_key_base = secure_token
