67f905288fe820013d5cda2f0ab785674acd14d9
katorie <katoriexxxkatorie@gmail.com>
2014-10-13 15:01:39 +0900

    Add login and permission

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  before_filter :get_feeds
+
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+  private
+  def get_feeds
+    @feeds = Feed.all
+    @items = Item.includes(:feed).order('posted_at desc').page(params[:page])
+  end
+
+end
