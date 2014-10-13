738c1641a8b0dfdcc6a29e01d3b28c645076964c
katorie <katoriexxxkatorie@gmail.com>
2014-10-13 14:22:21 +0900

    Add before_filter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,19 @@
 class SessionsController < Devise::SessionsController
 
+  before_filter :get_feeds
+
   def new
-    @feeds = Feed.all
-    @items = Item.includes(:feed).order('posted_at desc').page(params[:page])
     super
   end
 
   def create
+    super
+  end
+
+  private
+  def get_feeds
     @feeds = Feed.all
     @items = Item.includes(:feed).order('posted_at desc').page(params[:page])
-    super
   end
 
 end
