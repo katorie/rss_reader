e80484efb3c57cd9119a8b3cae62d89bde22272b
Yuka Kato <satoimo.to.imo@gmail.com>
2016-09-13 17:49:19 +0900

    Fix factory

--- spec/factories/feed.rb
+++ spec/factories/feed.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :feed do
     title    'test_feed_title'
-    feed_url FFaker::Internet.http_url
-    url      FFaker::Internet.http_url + '/atom.xml'
+    feed_url FFaker::Internet.http_url + '/blog/rss'
+    url      FFaker::Internet.http_url
     author   'alice'
 
     to_create do |instance|
